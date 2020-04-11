5babc356846ef52ea5714bbadb19bd0db508f3c2
Ben Kelly <b-kelly@users.noreply.github.com>
2020-04-11 17:57:07 -0400

    add button group tag helper

---
+++ src/Stacks.TagHelpers/Components/ButtonGroupTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Stacks.TagHelpers.Components
+{
+    //TODO move to Button file?
+    [RestrictChildren("button", "form", "a")]
+    public class ButtonGroupTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.AddClass("s-btn-group");
+        }
+    }
+
+    // TODO is this really necessary
+    [HtmlTargetElement("form", Attributes = "button-container", ParentTag = "button-group")]
+    public class ButtonGroupContainer : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.AddClass("s-btn-group--container");
+        }
+    }
+}
