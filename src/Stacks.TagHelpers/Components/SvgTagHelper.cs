f63937621ad39facda23e2a4f02696c15bd960e5
Ben Kelly <b-kelly@users.noreply.github.com>
2020-06-20 13:19:35 -0400

    add naive caching and some extra protection against bad inputs

--- src/Stacks.TagHelpers/Components/SvgTagHelper.cs
+++ src/Stacks.TagHelpers/Components/SvgTagHelper.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,7 +11,11 @@ namespace Stacks.TagHelpers.Components
     [HtmlTargetElement("s-svg")]
     public class SvgTagHelper : TagHelper
     {
+        private const string INVALID_PATH_CONTENT = "<span>Invalid svg name/path</span>";
+        private const string INVALID_CONFIG_CONTENT = "<span>Invalid SvgTagHelperConfiguration.SvgFolderPath</span>";
+
         private readonly SvgTagHelperConfiguration _config;
+        private static readonly ConcurrentDictionary<string, string> _cache = new ConcurrentDictionary<string, string>();
 
         public string Name { get; set; }
 
@@ -21,31 +27,68 @@ namespace Stacks.TagHelpers.Components
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ArgumentNullException(nameof(Name));
+            }
+
+            // cache the entire svg, which is generated based on Name + added classes
+            var hasClassAttribute = output.Attributes.TryGetAttribute("class", out var classes);
+            var cacheKey = Name + (hasClassAttribute ? "_" + classes.Value : "");
+
             // completely replace this tag with the contents
             output.TagName = null;
 
+            // if the item is in the cache, set and return early
+            if (_cache.ContainsKey(cacheKey))
+            {
+                output.Content.SetHtmlContent(_cache[cacheKey]);
+                return;
+            }
+
+            // if the config is invalid, return early
+            // TODO throwing might be better?
+            if (string.IsNullOrEmpty(_config?.SvgFolderPath))
+            {
+                _cache[cacheKey] = INVALID_CONFIG_CONTENT;
+                output.Content.SetHtmlContent(INVALID_CONFIG_CONTENT);
+                return;
+            }
+
             var path = Path.Combine(_config.SvgFolderPath, Name + ".svg");
 
+            // if the icon can't be loaded from disk, return early
+            // TODO throwing might be better?
             if (!File.Exists(path))
             {
-                output.Content.SetHtmlContent("<span>Invalid svg!</span>");
+                _cache[cacheKey] = INVALID_PATH_CONTENT;
+                output.Content.SetHtmlContent(INVALID_PATH_CONTENT);
                 return;
             }
 
             // TODO there is likely a better way to do this
-            // TODO cache the content so we don't have to re-read from disk every time
             var svg = File.ReadAllText(path);
 
-            // set classes
-            if (output.Attributes.ContainsName("class"))
+            // set classes onto the svg string
+            // TODO support svgs that don't have a class or the class attribute exists, but is not on the top level element
+            if (hasClassAttribute)
             {
-                output.Attributes.TryGetAttribute("class", out var classes);
                 svg = Regex.Replace(svg, @"class=""(.+?)""", $@"class=""$1 {classes.Value}""");
             }
+
+            // set the cache entry
+            _cache[cacheKey] = svg;
 
             output.Content.SetHtmlContent(svg);
         }
 
+        /// <summary>
+        /// Renders an icon's svg string
+        /// </summary>
+        /// <param name="config">The SvgTagHelperConfiguration to base our SvgTagHelper on</param>
+        /// <param name="iconName">The name of the icon to render</param>
+        /// <param name="classes">The classes to add to the icon's class attribute</param>
+        /// <returns></returns>
         public static async Task<string> RenderAsync(SvgTagHelperConfiguration config, string iconName, string classes)
         {
             var svg = new SvgTagHelper(config)
